eca5d605fb3179a3016f4f6904c4cf6edec0e541
Forecheng <lpctjut@163.com>
2016-05-31 22:51:43 +0800

    上拉刷新时的加载布局文件和自定义上拉刷新视图

---
+++ app/src/main/java/com/example/lpc/android_pulltorefresh_main/interfacee/ILoadingLayout.java
@@ -0,0 +1,8 @@
+package com.example.lpc.android_pulltorefresh_main.interfacee;
+
+/**
+ * Description: 加载布局的回调
+ * Created by lpc on 2016/5/31.
+ */
+public interface ILoadingLayout {
+}
---
+++ app/src/main/java/com/example/lpc/android_pulltorefresh_main/utils/Utils.java
@@ -0,0 +1,9 @@
+package com.example.lpc.android_pulltorefresh_main.utils;
+
+/**
+ * Description:
+ * Created by lpc on 2016/5/31.
+ */
+public class Utils {
+
+}
